677dc90ca6bb26710f8b352b29099bbacd9117de
jdalpert <jdalpert@github.com>
2016-06-16 14:53:11 -0700

    Added child with textboxes

--- src/components/child.react.js
+++ src/components/child.react.js
@@ -2,13 +2,38 @@ import React, { Component } from 'react';
 
 export default class Child extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+				name:"",
+				address:"",
+				quote: "",
+		};
+	}
 
-  render() {
-    return (
-      <div>This is a child component</div>
-    );  
-  }
+	_handleChange = (event) =>{
+		let obj = {};
+		obj[event.target.name] = event.target.value;
+		this.setState(obj);
+	}
+
+	_handleClick = () =>{
+		let obj = this.state;
+		console.log(obj);
+	}
+
+	render() {
+		return (
+			<div className="child">
+				<input type="text" name="name" placeholder="name" 
+					onChange={this._handleChange}/><br/>
+				<input type="text" name="address" placeholder="address" 
+					onChange={this._handleChange}/><br/>
+				<input type="text" name="quote" placeholder="quote" 
+					onChange={this._handleChange}/><br/>
+
+				<button type="button" onClick={this._handleClick}>Submit</button>
+			</div>
+		);  
+	}
 }
